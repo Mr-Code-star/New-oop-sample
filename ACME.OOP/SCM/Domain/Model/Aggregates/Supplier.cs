6823fd4c909fb814efdc881b9334f4a4c37a65e8
GabrielaShapiama28 <u202319448@upc.edu.pe>
2025-04-08 16:30:55 -0500

    feat(supplier): add supplier aggregate root.

--- ACME.OOP/SCM/Domain/Model/Aggregates/Supplier.cs
+++ ACME.OOP/SCM/Domain/Model/Aggregates/Supplier.cs
@@ -2,8 +2,17 @@ using ACME.OOP.Shared.Domain.Model.ValueObjects;
 
 namespace ACME.OOP.SCM.Domain.Model.Aggregates;
 
+/// <summary>
+/// this class represents a supplier agregate in the bounded context of the Supply Chain Management
+/// </summary>
+/// <param name="identifier"></param>
+/// <param name="name"></param>
+/// <param name="address"></param>
+
+
+
 //Esto ya es una clase
-public class Aggregate(string identifier, string name, Address address)
+public class Supplier(string identifier, string name, Address address)
 {
     public string Identifier { get; } = identifier ?? throw new ArgumentNullException(nameof(identifier));
     public string Name { get; } = name ?? throw new ArgumentNullException(nameof(name));
