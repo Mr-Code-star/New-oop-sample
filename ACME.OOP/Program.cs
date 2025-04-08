c0efdb09401d942226b623bc10e5a4fb62be2dd3
GabrielaShapiama28 <u202319448@upc.edu.pe>
2025-04-08 17:53:24 -0500

    feat(main): main behaivord.

--- ACME.OOP/Program.cs
+++ ACME.OOP/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ACME.OOP.Procurement.Domain.Model.Aggregates;
+using ACME.OOP.Procurement.Domain.Model.ValueObjects;
+using ACME.OOP.SCM.Domain.Model.Aggregates;
+using ACME.OOP.SCM.Domain.Model.ValueObjects;
+using ACME.OOP.Shared.Domain.Model.ValueObjects;
 
-Console.WriteLine("Hello, World!");
+var suplierAddres = new Address("Main St", "123456", "New York", "New York", "NY", "USA");
+var supplier = new Supplier("SUP0001","Microsfot, Inc", suplierAddres);
+
+// Create a purchaseorder object
+var purchaseorder = new PurchaseOrder("P0001",new SupplierId(supplier.Identifier),"USD");
+purchaseorder.AddItem(ProductID.New(), 10,25.99m);
+purchaseorder.AddItem(ProductID.New(), 5,15.99m);
+
+// Display the purchase order details
+Console.WriteLine($"Purchase Order ID: {purchaseorder.OrderNumber} created on {purchaseorder.OrderDate} for supplier {supplier.Name}");
+foreach (var item in purchaseorder.Items)
+{
+    var itemSubtotal = item.CalculateSubtotal();
+    Console.WriteLine($"Item Subtotal: {itemSubtotal.Amount:C} {itemSubtotal.Currency}");
+}
+
+var total = purchaseorder.CalculateSubtotal();
+Console.WriteLine($"Total Subtotal: {total.Amount:C} {total.Currency}");
+
